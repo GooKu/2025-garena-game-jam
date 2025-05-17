49ae0133b61bae8b4585c1d3220b561ce9881bb6
Goo <kuwoods@hotmail.com>
2025-05-17 14:47:47 +0800

    Implement GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using GModule;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    private int score;
+
+    private void Awake()
+    {
+        if (instance != null) 
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        MessageEventSystem.Register(EventKey.UpdateScore, updateScoreHandle);
+    }
+
+    private void newGame(string arg1, object[] arg2)
+    {
+        score = 0;
+        SceneManager.LoadScene("Stage01");
+    }
+
+    private void updateScoreHandle(string arg1, object[] arg2)
+    {
+        int deltaScore = (int)arg2[0];
+        score += deltaScore;
+    }
+}
