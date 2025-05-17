2ed58783b78ae4d6fa9c880ac7b838535ccd70ec
Goo <kuwoods@hotmail.com>
2025-05-17 13:04:09 +0800

    Implement level 01 Start anim

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
 using UnityEngine.Playables;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PlayableDirector director;
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private List<GameObject> optionGroup;
 
     private int score;
 
     void Start()
     {
-        foreach(var at in GameObject.FindObjectsByType<ActionToken>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+        targetObject.gameObject.SetActive(false);
+        for (int i = 0; i < optionGroup.Count; i++)
+        {
+            optionGroup[i].SetActive(false);
+        }
+        foreach (var at in GameObject.FindObjectsByType<ActionToken>(FindObjectsInactive.Include, FindObjectsSortMode.None))
         {
             at.ActionEvent += actionHandle;
         }
@@ -19,5 +27,14 @@ public class GameManager : MonoBehaviour
     {
         director.Play(actionToken.Result);
         score += actionToken.Score;
+    }
+
+    public void ShowOptions(int index)
+    {
+        targetObject.gameObject.SetActive(true);
+        for (int i = 0; i < optionGroup.Count; i++)
+        {
+            optionGroup[i].SetActive(i == index);
+        }
     }
 }
